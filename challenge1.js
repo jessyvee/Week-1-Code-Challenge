aadebeae086184a912c41e3b2ac8c1467f9b7a14
Jesse <jessyvee1@gmail.com>
2023-03-19 23:27:35 +0300

    completed assignment

---
+++ challenge1.js
@@ -0,0 +1,22 @@
+//code challenge 1
+const studentMarks = marks;{
+    //let marks =0;
+        console.log("mark");
+    
+        if (marks > 79){
+            console.log("Grade : A");      
+            } 
+          else if (marks > 60) {
+                  console.log("Grade : B"); 
+                    } 
+          else if (marks >49) {
+            console.log("Grade : c");
+          }
+          else if (marks >40) {
+            console.log("Grade : D");
+          }
+          else (mark<40) {
+          console.log("Grade : E ");
+          }
+        }
+    
