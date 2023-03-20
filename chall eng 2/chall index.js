4f3ed35fb2c70f64c3d368f62b7df139d1ea9db4
Jesse <jessyvee1@gmail.com>
2023-03-20 08:53:03 +0300

    completed assignment

---
+++ chall eng 2/chall index.js
@@ -0,0 +1,14 @@
+//code challenge 2
+const inputspeed ='speed'
+function speed(){
+    if(speed<70){
+     console.log = "ok";
+    }
+    else if("Points:"+((speed-70)/5)){
+        console.log(points)
+    }
+    else(((speed-70)/5)>12){
+        console.log('License suspended');
+    }
+    speed();
+}
