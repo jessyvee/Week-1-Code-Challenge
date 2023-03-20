23ea1f3289b51eeb5d712c9faae434608e7739f2
Jesse <jessyvee1@gmail.com>
2023-03-20 19:19:25 +0300

    completed assignment

---
+++ challeng 3/Challege 3.js
@@ -0,0 +1,12 @@
+//Calculate net sales 3 work
+function netsalary(float){
+    return bs - (tax+NHIF+NSSF);
+    let bs=30000
+    
+    console.log (tax=bs*0.25);
+    NSSF=bs*0.25;
+    NHIF=900;
+        
+    netsalary=Bs-(tax+NHIF+NSSF);
+    console.log(netsalary)
+}
